e1196f0e62a08ecc499e85fd82931d1d5733672d
Tanzeel <tanzeelsaleem10@gmail.com>
2024-09-09 02:04:46 +0500

    added the google analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,9 @@ import localFont from "next/font/local";
 import Navbar from "@/components/Navbar";
 import { NavbarForSmScreens } from "@/components/NavbarForSmScreens";
 import Footer from "@/components/Footer";
-import Head from 'next/head';
+import Head from "next/head";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,29 +18,53 @@ const FontDevWeb = localFont({
 
 export const metadata = {
   title: "Custom Software & Web Applications for Business Growth | DevNexus ",
-  description: "DevNexus specializes in creating custom software and web applications designed to fuel growth and success for your business. Boost your business with innovative digital solutions.",
+  description:
+    "DevNexus specializes in creating custom software and web applications designed to fuel growth and success for your business. Boost your business with innovative digital solutions.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <title> Custom Software & Web Applications for Business Growth | DevNexus</title>
-        <meta name="description" content="DevNexus specializes in creating custom software and web applications designed to fuel growth and success for your business. Boost your business with innovative digital solutions." />
-      
-          <script
+        <title>
+          {" "}
+          Custom Software & Web Applications for Business Growth | DevNexus
+        </title>
+        <meta
+          name="description"
+          content="DevNexus specializes in creating custom software and web applications designed to fuel growth and success for your business. Boost your business with innovative digital solutions."
+        />
+
+        {/* Open Graph Meta Tags */}
+        {/* <meta
+          property="og:title"
+          content="Custom Software & Web Applications for Business Growth | DevNexus"
+        />
+        <meta
+          property="og:description"
+          content="DevNexus specializes in creating custom software and web applications designed to fuel growth and success for your business. Boost your business with innovative digital solutions."
+        />
+        <meta
+          property="og:image"
+          content="https://www.devnexus.com/images/preview-image.jpg"
+        />
+        <meta property="og:url" content="https://www.devnexus.com" />
+        <meta property="og:type" content="website" /> */}
+
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "https://schema.org",
               "@type": "WebSite",
-              "name": "DevNexus",
-              "url": "https://www.devnexus.com",
-              "description": "DevNexus specializes in creating custom software and web applications designed to fuel growth and success for your business. Boost your business with innovative digital solutions."
+              name: "DevNexus",
+              url: "https://www.devnexus.com",
+              description:
+                "DevNexus specializes in creating custom software and web applications designed to fuel growth and success for your business. Boost your business with innovative digital solutions.",
             }),
           }}
         />
-      
+
       </Head>
 
       <body className={`${inter.className} ${FontDevWeb.variable} `}>
@@ -50,6 +76,7 @@ export default function RootLayout({ children }) {
 
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-FMYDM52P5F" />
     </html>
   );
 }
